ab81af23034a6916a135c8d0db02e6e52bb4d16d
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-02 10:36:44 -0600

    adds templates, layout and redirect

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 # SETUP: https://realpython.com/flask-by-example-part-1-project-setup/
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+todos = []
+
 @app.route("/")
 def tasks():
-    return "TASKS"
+    return render_template("tasks.html") 
 
-@app.route("/add")
+@app.route("/add", methods=["GET","POST"])
 def add():
-    return "ADD A NEW TRACK"
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        todos.append(todo)
+        return redirect("/")
+
 
 if __name__ == '__main__':
     app.run()
