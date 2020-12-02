0cd60732a3084fa79bbd4617b0e67948d110dc1a
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-02 10:46:47 -0600

    adds error handling and value passing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ todos = []
 
 @app.route("/")
 def tasks():
-    return render_template("tasks.html") 
+    return render_template("tasks.html", todos=todos) 
 
 @app.route("/add", methods=["GET","POST"])
 def add():
